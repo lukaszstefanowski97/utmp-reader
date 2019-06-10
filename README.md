dda05fb5411fe570544f5697193c0b199888e500
lukaszstefanowski97 <lukasz.stefanowski97@gmail.com>
2019-06-10 15:26:57 +0200

    utmp reader

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # utmp-reader
+
+This script is getting the content of Linux var/run/utmp file in order to iterate through all of the user processes and print the information.
+
+<b>Possible arguments:</b>
+
+-h shows hosts
+
+-l shows ut_line
+
+You can also run script without arguments and see just usernames, or use both at once.
+
+<b>To run the script please type</b>
+
+
+    make utmp_reader
+
+
+And then type
+
+
+    ./utmp_reader (optional arguments)
+  
+  
+Of course this script is going to work only on Linux OS. :)
--- utmp_reader.c
+++ utmp_reader.c
@@ -1,43 +1,40 @@
 #include <stdio.h>
 #include <utmp.h>
 #include <getopt.h>
-#include <stdlib.h>
-#include <pwd.h>
-#include <grp.h>
 
 int main(int argc, char *argv[]) {
-        int optionIndex = 0;
-        struct utmp *utmpEntry;
-        int hFlag = 0;
-        int lFlag = 0;
+    int optionIndex = 0;
+    struct utmp *utmpEntry;
+    int hFlag = 0;
+    int lFlag = 0;
 
-        while ((optionIndex = getopt(argc, argv, "hl")) != -1) {
-                switch(optionIndex)
-                {
-                        case 'h':
-                                hFlag = 1;
-                                break;
-                        case 'l':
-                                lFlag = 1;
-                                break;
-                        default:
-                                printf("ERROR: Unknown option\nPlease type -h for hosts and -l for groups.\n");
-                                return 1;
-                }
+    while ((optionIndex = getopt(argc, argv, "hl")) != -1) {
+        switch(optionIndex)
+        {
+            case 'h':
+                hFlag = 1;
+                break;
+            case 'l':
+                lFlag = 1;
+                break;
+            default:
+                printf("ERROR: Unknown option\nPlease type -h for hosts and -l for groups.\n");
+                return 1;
         }
+    }
 
-        setutent();
+    setutent();
+    utmpEntry = getutent();
+
+    while(utmpEntry) {
+        if(utmpEntry->ut_type==USER_PROCESS) {
+            if (hFlag == 0 && lFlag == 0) printf("%9s\n", utmpEntry->ut_user);
+            if (hFlag == 1 && lFlag == 0) printf("%9s (%s)\n", utmpEntry->ut_user, utmpEntry->ut_host);
+            if (hFlag == 0 && lFlag == 1) printf("%9s%12s\n", utmpEntry->ut_user, utmpEntry->ut_line);
+            if (hFlag == 1 && lFlag == 1) printf("%9s (%s)%12s\n", utmpEntry->ut_user, utmpEntry->ut_host, utmpEntry->ut_line);
+        }
         utmpEntry = getutent();
-        groupid = malloc()malloc(counter * sizeof (gid_t));
-        while(utmpEntry) {
-                if(utmpEntry->ut_type==USER_PROCESS) {
-                        if (hFlag == 0 && lFlag == 0) printf("%9s\n", utmpEntry->ut_user);
-                        if (hFlag == 1 && lFlag == 0) printf("%9s (%s)\n", utmpEntry->ut_user, utmpEntry->ut_host);
-                        if (hFlag == 0 && lFlag == 1) printf("%9s%12s\n", utmpEntry->ut_user, utmpEntry->ut_id);
-                        if (hFlag == 1 && lFlag == 1) printf("%9s (%s)%12s\n", utmpEntry->ut_user, utmpEntry->ut_host, utmpEntry->ut_line);
-                }
-                utmpEntry = getutent();
-        }
+    }
 
-        return 0;
+    return 0;
 }
